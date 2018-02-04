bfa67450c147b9c6341ca283ab878bc84b883603
ChristophEMaurer <35205064+ChristophEMaurer@users.noreply.github.com>
2018-02-04 15:20:15 +0100

    what a dumb editor!

--- operationen/src/Setup/Versionen/V1.29.19-chirurgie-x86/OP-LOG-V1.29.19/files/addtrust.bat
+++
@@ -1,5 +0,0 @@
-c:
-cd %windir%\Microsoft.NET\Framework\v2.0.50727
-caspol.exe -q -machine -addgroup "All_Code" -url "$__PLACEHOLDER__PROGRAM__$\*" FullTrust -n "oplog"
-caspol.exe -q -machine -addgroup "All_Code" -url "$__PLACEHOLDER__DATA__$\*" FullTrust -n "oplogData"
-
